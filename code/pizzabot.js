82ced5b962f76730141d284063785b0da2b33401
jonna ru <jonna.ru@icloud.com>
2020-09-11 23:11:34 +0200

    Re-styled in CSS and added two more pizzaz in HTML and CSS

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,14 +1,12 @@
-
 // *** declairing variables
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 const pizzaPrice = 80;
 
-const pizzaVegImg = document.getElementById("veg");
-const pizzaHawImg = document.getElementById("haw");
-const pizzaPepImg = document.getElementById("pep");
-
+// const pizzaVegImg = document.getElementById("veg");
+// const pizzaHawImg = document.getElementById("haw");
+// const pizzaPepImg = document.getElementById("pep");
 
 /*** Function 1 - Order Name 
     const validateOrderName = (order) => {
@@ -23,18 +21,18 @@ const pizzaPepImg = document.getElementById("pep");
 // *** Function 2 - Total Cost
 const calculateTotalCost = (quantity, price) => {
   return quantity * price;
-}
+};
 
 // *** Function 3 - Cooking Time
 const calculateCookingTime = (time) => {
   if (time <= 2) {
     return 10;
-  } else if (time >= 3 && time <=5) {
+  } else if (time >= 3 && time <= 5) {
     return 15;
   } else {
     return 20;
   }
-}
+};
 
 // *** Function - Submit form
 
@@ -46,27 +44,27 @@ const handleSubmit = (event) => {
 
   const pizzaType = document.forms.pizzaForm.elements.pizza.value;
   const orderQuantity = document.forms.pizzaForm.numberOfPizzas.value;
-  
-    const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
-    const cookingTime = calculateCookingTime(orderQuantity);
 
-    // 'ternary operator' (puts the 's for plural number of pizzas)
-    const plural = orderQuantity > 1 ? "'s" : "";
+  const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+  const cookingTime = calculateCookingTime(orderQuantity);
 
-    document.getElementById(
-      "orderSummery"
-    ).innerHTML = `Hello ${userName}! I'll get started on your ${orderQuantity} ${
-      pizzaType + plural
-    } right away. It will cost ${orderTotal} kr and will take ${cookingTime} minutes.`;
-    
-    if (pizzaType === "Vegetarian Pizza") {
-      pizzaVegImg.setAttribute ("style", "display:block;");
-    } else if (pizzaType === "Hawaiian Pizza") {
-      pizzaHawImg.setAttribute ("style", "display:block;")
-    } else if (pizzaType === "Pepperoni Pizza") {
-      pizzaPepImg.setAttribute ("style", "display:block;")
-    }
-}; 
+  // 'ternary operator' (puts the 's for plural number of pizzas)
+  const plural = orderQuantity > 1 ? "'s" : "";
+
+  document.getElementById(
+    "orderSummery"
+  ).innerHTML = `Hello ${userName}! I'll get started on your ${orderQuantity} ${
+    pizzaType + plural
+  } right away. It will cost ${orderTotal} kr and will take ${cookingTime} minutes.`;
+
+  // if (pizzaType === "Vegetarian Pizza") {
+  //   pizzaVegImg.setAttribute ("style", "display:block;");
+  // } else if (pizzaType === "Hawaiian Pizza") {
+  //   pizzaHawImg.setAttribute ("style", "display:block;")
+  // } else if (pizzaType === "Pepperoni Pizza") {
+  //   pizzaPepImg.setAttribute ("style", "display:block;")
+  // }
+};
 
 /*  
 alert(
@@ -84,12 +82,12 @@ if (validateOrderName(orderName) === true) {
   const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
   const cookingTime = calculateCookingTime(orderQuantity);
 
-  document.getElementById("orderSummery").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`;
+  document.getElementById(
+    "orderSummery"
+  ).innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`;
   ///
-//} else {
-//  alert('Select a pizza from the menu');
+  //} else {
+  //  alert('Select a pizza from the menu');
 }
 
-
 /// How to create a countdown timer: https://www.w3schools.com/howto/howto_js_countdown.asp
-
